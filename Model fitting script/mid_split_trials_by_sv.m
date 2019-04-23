d0713d287674829a3cc5434280e58d058f1b6552
ruonanjia <ruonjia@gmail.com>
2019-04-23 15:00:34 -0400

    Label trials mid-split by sv

--- Model fitting script/mid_split_trials_by_sv.m
+++ Model fitting script/mid_split_trials_by_sv.m
@@ -7,18 +7,13 @@
 % - Fitted parameters, should be a data sheet
 % - Subject log .mat
 % Output:
-% - SV per trial
-% - High/Low labeling 
+% - SV per trial, Data.sv
+% - SV median, Data.sv_median
+% - High/Low labeling based on value: Data.sv_label_val
+% - High/Low labeling based on saliency: Data.sv_label_sal
 
 %%
 clearvars
-
-fitparwave = 'Behavior data fitpar_04232019';
-% outputwave = '_04232019';
-isconstrained = 2;
-% exclude should match those in the fit_parameters.m script
-exclude = [77 1218]; 
-% TEMPORARY: subjects incomplete data (that the script is not ready for)
 
 %% folder and subjects
 % root = 'D:\Ruonan\Projects in the lab\VA_RA_PTB\Analysis Ruonan';
@@ -31,17 +26,17 @@ exclude = [61 76 78 79 80 81 95 100 101 102 104 117 1210 1220 1234 1235 1250 125
 
 subjects = subjects(~ismember(subjects, exclude));
 
+% test
+% subjects = [125];
+
+fitparwave = 'Behavior data fitpar_04232019';
 path_out = fullfile(root, 'Fitpar files', fitparwave, filesep);
 
-domains = [1, 0];
+if ~exist(path_out)
+    mkdir(path_out)
+end
 
-% % defining monetary values
-% valueP = [4 5 6 7 8 10 12 14 16 19 23 27 31 37 44 52 61 73 86 101 120];
-% value = repmat(valueP,6,1);
-% valueN = [-4 -5 -6 -7 -8 -10 -12 -14 -16 -19 -23 -27 -31 -37 -44 -52 -61 -73 -86 -101 -120];
-% % six risk and ambig levels
-% probs = [0.25; 0.5; 0.75; 0.5; 0.5; 0.5];
-% ambigs = [0; 0; 0; 0.24; 0.5; 0.74];
+domains = [1, 0];
 
 par = readtable(fullfile(root,'Clinical and behavioral/par nonpar att_allSubj_03202019.xlsx'));
 
@@ -66,22 +61,22 @@ for subject_idx = 1:length(subjects)
         
         % Pick the domain to analyze
         if (is_gains)
-          data = gdata;
+          Data = gdata;
         else
-          data = ldata;
+          Data = ldata;
         end
 
         % model fitted parameters for calculating subjective value
-        alpha = par.alpha(par.isGain == is_gains);
-        beta = par.beta(par.isGain == is_gains);
+        alpha = par.alpha(par.isGain == is_gains & par.id == subjectNum);
+        beta = par.beta(par.isGain == is_gains & par.id == subjectNum);
 
         %% Compute subjective value of each choice
         % Use the best fit for every subjects (most should be unconstrained, use constrained for a few subjects)
-        for reps = 1:length(data.choice)
+        for reps = 1:length(Data.choice)
           sv(reps, 1) = ambig_utility(0, ...
-              data.vals(reps), ...
-              data.probs(reps), ...
-              data.ambigs(reps), ...
+              Data.vals(reps), ...
+              Data.probs(reps), ...
+              Data.ambigs(reps), ...
               alpha, ...
               beta, ...
               'ambigNrisk');
@@ -96,8 +91,30 @@ for subject_idx = 1:length(subjects)
         
         % label mid-split high/low trials
         % find median
+        sv_median = median(Data.sv);
+        Data.sv_median = sv_median;
+        % label mid-split, based on value
+        Data.sv_label_val(Data.sv <= sv_median)  = 0;
+        Data.sv_label_val(Data.sv > sv_median) = 1;
+        
+        % label mid-split, based on saliency
+        if is_gains
+            Data.sv_label_sal(Data.sv <= sv_median)  = 0;
+            Data.sv_label_sal(Data.sv > sv_median) = 1; 
+        elseif ~is_gains
+            Data.sv_label_sal(Data.sv <= sv_median)  = 1;
+            Data.sv_label_sal(Data.sv > sv_median) = 0;
+        end
+        
+        if is_gains
+            domain = 'GAINS';
+        elseif ~is_gains
+            domain = 'LOSS';
+        end
+        
+        % save data into fitpar
+        save(fullfile(path_out, ['RA_' domain '_' num2str(subjectNum) '_fitpar.mat']), 'Data')
         
     end
 end
-        
         
